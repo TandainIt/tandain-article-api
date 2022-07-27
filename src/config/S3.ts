8f1f8d641e84389318e6963e86337ef2b848af18
Peitra Erdi <peitrae@gmail.com>
2022-07-27 20:06:55 +0700

    Change aws-sdk usage to  @aws-sdk/client-s3

--- src/config/S3.ts
+++ src/config/S3.ts
@@ -1,9 +1,7 @@
-import AWS from 'aws-sdk';
+import { S3 } from "@aws-sdk/client-s3"; 
 
-// NOTE: Set the region 
-AWS.config.update({region: 'ap-northeast-1'});
 
 // NOTE: Create S3 service object
-const s3 = new AWS.S3({apiVersion: 'latest'});
+const s3 = new S3({apiVersion: 'latest', region: 'ap-northeast-1'});
 
 export default s3
